ca6ff5e30d6387cfeea2974196a0f76a96b06745
Jamie ONeill <joneill@sembracare.com>
2019-08-20 16:47:35 -0400

    adding javascript that puts userGuess into wrongGuess variable but it still isn't printing correctly

--- assets/javaScript/game.js
+++ assets/javaScript/game.js
@@ -1,6 +1,6 @@
 //Variables that hold references to places in the HTML - this isn't working
 var hiddenDisplay = document.getElementById("hidden_answer");
-// var userChoiceText = document.getElementById("userchoice_text");
+var wrongDisplay = document.getElementById("wrong_text");
 
 //Starting simple but this have more answers later
 var computerAnswer = ["cartman", "bobby", "sally"];
@@ -28,22 +28,41 @@ console.log(hiddenAnswerToDisplay.length);
 console.log("hiddenAnswerToDisplay type is "+typeof hiddenAnswerToDisplay);
 console.log("the last in the object is a type of " + typeof hiddenAnswerToDisplay[3]);
 
-
+//Creating a variable that is an array to store wrong Guess in
+var wrongGuess = [];
 
 // This function is run whenever the user presses a key.
 document.onkeyup = function(event) {
-    // Determines which key was pressed.
+    // Determines which key was pressed and assigns to userGuess.
     var userGuess = event.key;
+    console.log("the user pressed this key " +userGuess);
+
+
+
+    //Creating an array variable to store positions of correct guess
+    var s = [];
 
     // This code will determine the position of the letters in the computer answer that match the user guess
-    // var s = [];
     for (index = 0; index < charComputerAnswer.length; index++) {
         if (userGuess === charComputerAnswer[index]) {
             hiddenAnswerToDisplay.splice(index, 1, userGuess);
+            s.push(index);
+            console.log("s in for-if loop is " + s);
+            // console.log("s is a type of" + typeof s);
         }
     }
+    if (s.length === 0) {
+        console.log('s out of the for-if loop' + s);
+        wrongGuess.push(userGuess);
+        console.log("wrong guess is " + wrongGuess);
+        //displaying wrong guesses to DOM isn't working
+        // wrongDisplay.innerHTML = wrongGuess;
+    }
+    console.log("hiddenAnswer to display looks like this outside the for loop " + hiddenAnswerToDisplay);
 
     // This displays the blanks to the user and replaces comma in an array with a space
     hiddenDisplay.innerHTML = hiddenAnswerToDisplay.join(" ");
+    // wrongDisplay.innerHTML = wrongGuess;
+
 }    
     
